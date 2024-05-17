c61961bb4c015885a3b97805a44dbf1ddcef4908
hishamkame <hishamkamel370@gmail.com>
2024-05-18 00:33:32 +0300

    adding flutter_slidable to delete item

--- lib/widgets/list_tile_item.dart
+++ lib/widgets/list_tile_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class ListTileItem extends StatelessWidget {
   const ListTileItem({
@@ -7,23 +8,37 @@ class ListTileItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 5),
-      decoration: BoxDecoration(
-        color: Colors.grey[400],
-        borderRadius: BorderRadius.circular(8),
+    return Slidable(
+      endActionPane: ActionPane(
+        motion: const StretchMotion(),
+        children: [
+          SlidableAction(
+            foregroundColor: Colors.black,
+            borderRadius: BorderRadius.circular(8),
+            onPressed: (context) {},
+            icon: Icons.delete,
+            backgroundColor: Colors.red.shade300,
+          ),
+        ],
       ),
-      child: ListTile(
-        title: const Text(
-          'data',
-          style: TextStyle(fontSize: 18),
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 5),
+        decoration: BoxDecoration(
+          color: Colors.grey[400],
+          borderRadius: BorderRadius.circular(8),
         ),
-        subtitle: Text(
-          '${DateTime.now().year} / ${DateTime.now().month} / ${DateTime.now().day}',
-        ),
-        trailing: const Text(
-          '\$35',
-          style: TextStyle(fontSize: 18),
+        child: ListTile(
+          title: const Text(
+            'data',
+            style: TextStyle(fontSize: 18),
+          ),
+          subtitle: Text(
+            '${DateTime.now().year} / ${DateTime.now().month} / ${DateTime.now().day}',
+          ),
+          trailing: const Text(
+            '\$35',
+            style: TextStyle(fontSize: 18),
+          ),
         ),
       ),
     );
