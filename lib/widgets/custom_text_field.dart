6a9736f268e27d217bb49c8c9d47edef030d029a
hishamkame <hishamkamel370@gmail.com>
2024-05-18 01:03:00 +0300

    validate data

--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -1,19 +1,28 @@
 import 'package:flutter/material.dart';
 
-class CustomTextField extends StatelessWidget {
-  const CustomTextField({
+class CustomTextFormField extends StatelessWidget {
+  const CustomTextFormField({
     super.key,
-    required this.hintText, this.textInputType = TextInputType.number,
+    required this.hintText,
+    this.textInputType = TextInputType.number,
+    this.onSaved,
   });
   final String hintText;
   final TextInputType textInputType;
+  final void Function(String?)? onSaved;
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      validator: (value) {
+        if (value!.isEmpty) {
+          return 'Field is required';
+        }
+        return null;
+      },
+      onSaved: onSaved,
       keyboardType: textInputType,
       decoration: InputDecoration(
         hintText: hintText,
-        
       ),
     );
   }
--- lib/widgets/show_aler_dialog.dart
+++ lib/widgets/show_aler_dialog.dart
@@ -2,40 +2,60 @@ import 'package:flutter/material.dart';
 
 import 'custom_text_field.dart';
 
-class ShowAlertDialog extends StatelessWidget {
+class ShowAlertDialog extends StatefulWidget {
   const ShowAlertDialog({
     super.key,
   });
 
   @override
+  State<ShowAlertDialog> createState() => _ShowAlertDialogState();
+}
+
+class _ShowAlertDialogState extends State<ShowAlertDialog> {
+  String? name;
+  double? dollars, cets;
+  GlobalKey<FormState> formKey = GlobalKey();
+  @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: const Text('Add new expense'),
-      content: const Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          CustomTextField(
-            textInputType: TextInputType.name,
-            hintText: 'Add new expense',
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: CustomTextField(
-                  hintText: 'Dollars',
+      content: Form(
+        key: formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            CustomTextFormField(
+              onSaved: (val) {
+                name = val;
+              },
+              textInputType: TextInputType.name,
+              hintText: 'Add new expense',
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: CustomTextFormField(
+                    hintText: 'Dollars',
+                    onSaved: (val) {
+                      dollars = double.parse(val!);
+                    },
+                  ),
                 ),
-              ),
-              SizedBox(
-                width: 10,
-              ),
-              Expanded(
-                child: CustomTextField(
-                  hintText: 'Cents',
+                const SizedBox(
+                  width: 10,
                 ),
-              ),
-            ],
-          ),
-        ],
+                Expanded(
+                  child: CustomTextFormField(
+                    onSaved: (val) {
+                      cets == null ? 0 : double.parse(val!);
+                    },
+                    hintText: 'Cents',
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
       actions: [
         ElevatedButton(
@@ -48,7 +68,11 @@ class ShowAlertDialog extends StatelessWidget {
         ),
         ElevatedButton(
           onPressed: () {
-            Navigator.pop(context);
+            if (formKey.currentState!.validate()) {
+              formKey.currentState!.save();
+              Navigator.pop(context);
+            }
+            
           },
           child: const Text(
             'Save',
