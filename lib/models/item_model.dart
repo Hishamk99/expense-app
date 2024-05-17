2f97ce3503c185cf1d861e51e869eac811dcfcb0
hishamkame <hishamkamel370@gmail.com>
2024-05-18 00:38:02 +0300

    create item model

---
+++ lib/models/item_model.dart
@@ -0,0 +1,11 @@
+class ItemModel {
+  final String name;
+  final String amount;
+  final DateTime date;
+
+  ItemModel({
+    required this.name,
+    required this.amount,
+    required this.date,
+  });
+}
