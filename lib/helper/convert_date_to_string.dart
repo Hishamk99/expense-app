80868c7d715321fb5e7ac481c5a6543e5c9734ba
hishamkame <hishamkamel370@gmail.com>
2024-05-22 21:18:06 +0300

    create getNameDay method

--- lib/helper/convert_date_to_string.dart
+++ lib/helper/convert_date_to_string.dart
@@ -11,3 +11,24 @@ String convertDateToString(DateTime date) {
   String yyyymmdd = year + month + day;
   return yyyymmdd;
 }
+
+String getNameDay(DateTime date) {
+  switch (date.weekday) {
+    case 1:
+      return 'Sun';
+    case 2:
+      return 'Mon';
+    case 3:
+      return 'Tue';
+    case 4:
+      return 'Wed';
+    case 5:
+      return 'Thur';
+    case 6:
+      return 'Fri';
+    case 7:
+      return 'Sat';
+    default:
+      return '';
+  }
+}
