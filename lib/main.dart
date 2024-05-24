3a6a50af030f13cde48dd75a8b238fe359917440
hishamkame <hishamkamel370@gmail.com>
2024-05-24 22:05:21 +0300

    init hive and open box

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:expenses_app/models/item_model.dart';
 import 'package:expenses_app/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 
 import 'cubit/expense_data_cubit.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(ItemModelAdapter());
+  await Hive.openBox('expense');
+  
   runApp(const ExpenseApp());
 }
 
