aa87c754834aae0de90df4d19920f1a9551aa522
hishamkame <hishamkamel370@gmail.com>
2024-05-24 21:57:49 +0300

    generate item_model.g

--- lib/models/item_model.dart
+++ lib/models/item_model.dart
@@ -1,6 +1,13 @@
-class ItemModel {
+import 'package:hive/hive.dart';
+part 'item_model.g.dart';
+
+@HiveType(typeId: 0)
+class ItemModel extends HiveObject {
+  @HiveField(0)
   final String name;
+  @HiveField(1)
   final double amount;
+  @HiveField(2)
   final DateTime date;
 
   ItemModel({
