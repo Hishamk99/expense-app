8147c9e4f8b5c6b60fe110f32424489c4262b723
hishamkame <hishamkamel370@gmail.com>
2024-05-25 12:55:08 +0300

    finish ExpensesCubit

--- lib/cubits/expense_cubit/expenses_cubit.dart
+++ lib/cubits/expense_cubit/expenses_cubit.dart
@@ -1,10 +1,19 @@
-
-
+import 'package:expenses_app/models/item_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 part 'expenses_state.dart';
 
 class ExpensesCubit extends Cubit<ExpensesState> {
   ExpensesCubit() : super(ExpensesInitial());
+
+
+  List<ItemModel>? expenses;
+  fetchAllExpenses() async {
+    var expenseBox = Hive.box<ItemModel>('expense');
+
+    expenses = expenseBox.values.toList();
+    emit(ExpensesSuccess());
+  }
 }
